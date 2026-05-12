42fed5d0d673f465f49bb7720aee0262d6712ca8
MarcoMendoza7 <mendozamarcope29@gmail.com>
2026-05-12 00:36:56 -0600

    Se agrego archvio nuevo.py interfaz del software

--- hacking_suite/pass_module.py
+++
@@ -1,8 +0,0 @@
-import random
-import string
-
-def generar_contrasena(longitud):
-    if longitud < 8:
-        raise ValueError("Mínimo 8 caracteres")
-    chars = string.ascii_letters + string.digits + string.punctuation
-    return ''.join(random.choice(chars) for _ in range(longitud))
